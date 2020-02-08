f803d2012971b1858e1a28b9bd276a1a2d4c3e6a
unknown <bouzid.souad@gmail.com>
2020-02-08 20:39:28 +0100

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-console.log('Client side javascript file is loading!')
 const weatherFrom = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
