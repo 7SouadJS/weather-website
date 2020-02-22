c67a47fa0f36ceabecb8a184021f5ac5f1e5a095
unknown <bouzid.souad@gmail.com>
2020-02-22 23:20:56 +0100

    Add high and low forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,8 @@ if (error) {
 } else if (body.error) {
    callback('Unable to find location', undefined)
 } else {
-   callback(undefined, body.daily.data[0].summary + ' It is currently '+ body.currently.temperature + ' degree out, There is a  '+ body.currently.precipProbability + '% chances of rain')
+   console.log(body.daily.data[0])
+   callback(undefined, body.daily.data[0].summary + ' It is currently '+ body.currently.temperature + ' degree out,This high today is '+ body.daily.data[0].temperatureHigh + ', with a low of '+ body.daily.data[0].temperatureLow + '. There is a ' + body.currently.precipProbability + '% chances of rain')
      }
    })
 }
